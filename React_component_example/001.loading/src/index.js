9537841f648617137086d97873b54ffac401a8d4
sungwoo1548 <swo1548@gmail.com>
2019-10-30 17:30:54 +0900

    init setting react app

---
+++ React_component_example/001.loading/src/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom';
+
+
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                hello
+            </div>
+        )
+    }
+}
+
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
